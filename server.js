356dd12a6a725784e7eacdd6c0ad8abe0025e38c
axeljk <axeljkern@yahoo.com>
2022-07-22 11:18:01 -0400

    Server test code added.

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = 3001;
+const app = express();
+
+app.
+
+
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+const path = require('path');
+
+// Serve images, css files, js files from the public directory
+// Allows us to reference files with their relative path
+// Example: http://localhost:3000/images/cat.jpg
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+
+app.listen(PORT, () =>
+  console.log(`Serving static asset routes on port ${PORT}!`)
+);
