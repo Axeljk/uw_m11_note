9a3c0f7f70078ab59ee7cc469008364ce71598e3
axeljk <axeljkern@yahoo.com>
2022-07-22 17:03:01 -0400

    Configures project to work on Heroku.

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const path = require("path");
 const express = require("express");
 
-const PORT = 3001;
+const PORT = precess.env.PORT || 3000;
 const app = express();
+
+// Handles static file requests.
+app.use(express.static("public"));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
 // GET Route for feedback page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
-
-// Serve the CSS file
-app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, '/public/assets/css/styles.css')));
-
-// Serve the JS file
-app.get("/assets/js/index.js", (req, res) => res.sendFile(path.join(__dirname, "public/assets/js/index.js")));
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
