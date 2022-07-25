df1b9adf22bd35e977cfb293bb6c5d094d3d6784
axeljk <axeljkern@yahoo.com>
2022-07-24 23:51:04 -0400

    Reorganizes code and adds comments.

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
-const router = express.Router();
 
 // Handles static file requests.
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static pages.
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
-// Handles routes.
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-router.use("/api/pets", require("./routes/notesRoute.js"));
+// Handles notes route.
+app.use("/api/notes", require("./routes/notesRoute"));
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
